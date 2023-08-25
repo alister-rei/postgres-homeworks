e5e2319e78bf471774c3579372e3f7285a6d1988
alister <lhadkov@mail.ru>
2023-08-25 22:47:18 +0300

    write command to create tabl

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers
+(
+    customer_id VARCHAR(20) PRIMARY KEY,
+    company_name VARCHAR(100) NOT NULL,
+    contact_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL,
+    notes TEXT
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id VARCHAR(20) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date DATE,
+	ship_city VARCHAR(100) NOT NULL
+);
